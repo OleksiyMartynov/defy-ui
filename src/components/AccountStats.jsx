6e177252afad072cdd51698ec2d274850311dfe0
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2020-05-03 08:30:03 -0400

    mobile support

--- src/components/AccountStats.jsx
+++ src/components/AccountStats.jsx
@@ -14,28 +14,33 @@ class AccountStats extends React.Component {
 
     return (
       <div className="AccountStats">
-        <div className="AccountStats__title">Funds</div>
-        <div className="AccountStats__total">999,999</div>
-        <div className="AccountStats__numbers-row">
-          <div className="AccountStats__numbers-row__subtitle">Available</div>
-          <div className="AccountStats__numbers-row__subtitle">Locked</div>
-        </div>
-        <div className="AccountStats__numbers-row">
-          <div className="AccountStats__numbers-row__number">49,999</div>
-          <div className="AccountStats__numbers-row__number">49,999</div>
+        <div className="AccountStats__rows-wrapper">
+          <div className="AccountStats__title">Funds</div>
+          <div className="AccountStats__total">999,999</div>
+
+          <div className="AccountStats__numbers-row">
+            <div className="AccountStats__numbers-row__subtitle">Available</div>
+            <div className="AccountStats__numbers-row__subtitle">Locked</div>
+          </div>
+          <div className="AccountStats__numbers-row">
+            <div className="AccountStats__numbers-row__number">49,999</div>
+            <div className="AccountStats__numbers-row__number">49,999</div>
+          </div>
         </div>
         <div className="AccountStats__divider" />
-        <div className="AccountStats__numbers-row">
-          <Button onClick={this.onDeposit}>
-            <i className="fa fa-arrow-circle-down" />
-            &nbsp;Deposit
-          </Button>
-        </div>
-        <div className="AccountStats__numbers-row">
-          <Button>
-            <i className="fa fa-arrow-circle-up" />
-            &nbsp;Withdraw
-          </Button>
+        <div className="AccountStats__rows-wrapper">
+          <div className="AccountStats__numbers-row">
+            <Button onClick={this.onDeposit}>
+              <i className="fa fa-arrow-circle-down" />
+              <span>&nbsp;Deposit</span>
+            </Button>
+          </div>
+          <div className="AccountStats__numbers-row">
+            <Button>
+              <i className="fa fa-arrow-circle-up" />
+              <span>&nbsp;Withdraw</span>
+            </Button>
+          </div>
         </div>
       </div>
     );
